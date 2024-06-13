99a408af50d85df66a6c4201d72f392a81a56480
bitbonk <bitbonk@msn.com>
2024-06-13 15:40:32 +0200

    Load Patch DLL

--- RuntimePatchingDemo.Patcher/MyPatcher.cs
+++ RuntimePatchingDemo.Patcher/MyPatcher.cs
@@ -1,5 +1,8 @@
 ﻿// your code, most likely in your own dll
 
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Loader;
 using HarmonyLib;
 
 public class MyPatcher
@@ -9,7 +12,15 @@ public class MyPatcher
 
     public static void DoPatching()
     {
+        var patchesAssembly = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "RuntimePatchingDemo.Patches.dll");
+
+        Console.WriteLine($"Loading patches assembly {patchesAssembly}");
+        
+        AssemblyLoadContext.Default.LoadFromAssemblyPath(patchesAssembly);
+        
         var harmony = new Harmony("com.example.patch");
+
+        Console.WriteLine($"Patching {new StackTrace().GetFrame(1).GetMethod().ReflectedType.Assembly.Location}");
         harmony.PatchAll();
     }
 }
